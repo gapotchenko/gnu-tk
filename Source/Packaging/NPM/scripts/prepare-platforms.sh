ad4c3aaa45bf5be2e84fcc31fdfc33c526ab9f39
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2025-07-14 17:07:22 +0200

    CI/CD in progress.

--- Source/Packaging/NPM/scripts/prepare-platforms.sh
+++ Source/Packaging/NPM/scripts/prepare-platforms.sh
@@ -5,15 +5,25 @@ rm -rf platforms && mkdir platforms
 
 basePath=../../../Gapotchenko.GnuTK/bin/Release/net9.0
 
+# Windows
+
 mkdir platforms/win-x64
 cp -l "$basePath/win-x64/publish/Gapotchenko.GnuTK.exe" platforms/win-x64/gnu-tk.exe
 
 mkdir platforms/win-arm64
 cp -l "$basePath/win-arm64/publish/Gapotchenko.GnuTK.exe" platforms/win-arm64/gnu-tk.exe
 
+# Linux
+
 mkdir platforms/linux-x64
 cp -l "$basePath/linux-x64/publish/Gapotchenko.GnuTK" platforms/linux-x64/gnu-tk
 chmod +x platforms/linux-x64/gnu-tk
+
+mkdir platforms/linux-arm64
+cp -l "$basePath/linux-arm64/publish/Gapotchenko.GnuTK" platforms/linux-arm64/gnu-tk
+chmod +x platforms/linux-arm64/gnu-tk
+
+# macOS
 
 mkdir platforms/osx-x64
 cp -l "$basePath/osx-x64/publish/Gapotchenko.GnuTK" platforms/osx-x64/gnu-tk
