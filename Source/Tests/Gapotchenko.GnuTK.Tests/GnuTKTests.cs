ecb67baed635588607159958dea792a50bcb2e71
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2025-07-20 16:35:59 +0200

    Tests in progress.

--- Source/Tests/Gapotchenko.GnuTK.Tests/GnuTKTests.cs
+++ Source/Tests/Gapotchenko.GnuTK.Tests/GnuTKTests.cs
@@ -4,9 +4,11 @@
 public class GnuTKTests
 {
     [TestMethod]
-    [DataRow("auto")]
-    public void ToolkitAvailability(string toolkit)
+    [DynamicData(nameof(GnuTK_TestData_Toolkits))]
+    public void GnuTK_Toolkit_Availability(string toolkit)
     {
 
     }
+
+    static IEnumerable<ValueTuple<string>> GnuTK_TestData_Toolkits => TestServices.EnumerateToolkits().Select(x => ValueTuple.Create(x));
 }
---
+++ Source/Tests/Gapotchenko.GnuTK.Tests/TestServices.cs
@@ -0,0 +1,43 @@
+﻿// Gapotchenko.GnuTK
+//
+// Copyright © Gapotchenko and Contributors
+//
+// File introduced by: Oleksiy Gapotchenko
+// Year of introduction: 2025
+
+using Gapotchenko.FX.Math.Intervals;
+using Gapotchenko.Shields.Cygwin.Deployment;
+using Gapotchenko.Shields.Homebrew.Deployment;
+using Gapotchenko.Shields.Microsoft.Wsl.Deployment;
+using Gapotchenko.Shields.MSys2.Deployment;
+
+namespace Gapotchenko.GnuTK.Tests;
+
+static class TestServices
+{
+    /// <summary>
+    /// Enumerates installed GNU toolkits to test.
+    /// </summary>
+    public static IEnumerable<string> EnumerateToolkits()
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            if (MSys2Deployment.EnumerateSetupInstances().Any())
+                yield return "msys2";
+            if (CygwinDeployment.EnumerateSetupInstances().Any())
+                yield return "cygwin";
+            if (WslDeployment.EnumerateSetupInstances(ValueInterval.FromInclusive(new Version(2, 0))).Any())
+                yield return "wsl";
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            if (BrewDeployment.EnumerateSetupInstances().Any())
+                yield return "brew";
+            yield return "system";
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            yield return "system";
+        }
+    }
+}
