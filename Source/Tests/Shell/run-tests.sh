9c13a65c523671babbf179f52baabf8be6d40d86
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2025-08-12 04:55:52 +0200

    Test formalization.

--- Source/Tests/Shell/run-tests.sh
+++ Source/Tests/Shell/run-tests.sh
@@ -2,10 +2,15 @@
 
 set -eu
 
-cd "$(dirname "$(readlink -fn "$0")")"
+GNU_TK_TEST_FILE_ASSET="${1-}"
+if [ -n "$GNU_TK_TEST_FILE_ASSET" ]; then
+    GNU_TK_TEST_FILE_ASSET="$(readlink -fn -- "$GNU_TK_TEST_FILE_ASSET")"
+fi
+
+cd "$(dirname "$(readlink -fn -- "$0")")"
 
 find . -type f -name "*.sh" -exec chmod +x {} \;
-export PATH=$PATH:$(pwd)/tools
+export PATH="$PATH:$(pwd)/tools"
 
 gnu-tk.sh --version
 gnu-tk.sh check -q
@@ -14,6 +19,14 @@ echo
 echo Running subtests...
 echo
 
-export GNU_TK_TEST_FILE_ASSET="${1-}"
+export GNU_TK_TEST_FILE_ASSET
 
-cd subtests && ./run.sh && cd ..
+(cd subtests && ./run.sh) || {
+    rc=$?
+    echo >&2
+    echo "TEST FAILED" >&2
+    exit "$rc"
+}
+
+echo
+echo "TEST PASSED"
