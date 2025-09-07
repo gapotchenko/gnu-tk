1c23108f1d735aa1b0c9143335f5a29660dd907f
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2025-09-07 07:04:52 +0200

    Do not use 'cmd.exe' to call 'gnu-tk' back from WSL because it distorts command-line arguments.

--- Source/Tests/Shell/tools/gnu-tk.sh
+++ Source/Tests/Shell/tools/gnu-tk.sh
@@ -3,11 +3,4 @@
 set -eu
 
 GNU_TK="${GNU_TK:-gnu-tk}"
-
-if [ -n "${WSL_DISTRO_NAME:-}" ]; then
-    # On WSL, call the Windows executable back.
-    cmd.exe /c "$GNU_TK" "$@"
-else
-    # On Unix, directly run the executable.
-    "$GNU_TK" "$@"
-fi
+"$GNU_TK" "$@"
