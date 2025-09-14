717f4075dd0e3e4cdb4142bf9d1c72d3bbbb4276
Oleksiy Gapotchenko <alex.s.gap@gmail.com>
2025-09-14 09:57:58 +0200

    Package .msi artifacts.

---
+++ Source/Packaging/Setup/build/pack.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+set -eu
+
+cd ..
+
+# ---------------------------------------------------------------------------
+
+repoPath="$(pwd)/../../.."
+version=$(xq "$repoPath/Source/Mastering/.NET/Version.props" -x //Project/PropertyGroup/Version)
+
+# ---------------------------------------------------------------------------
+
+get_output_file_name() {
+    version=$1
+    platform=$2
+    echo "gnu-tk-$version-setup-$platform"
+}
+
+pack_windows() {
+    platform=$1
+    arch=$2
+    lang="en-US"
+
+    output_file_name="$(get_output_file_name "$version" "$platform").msi"
+
+    echo "$output_file_name"
+    cp -l "$repoPath/Source/Setup/MSI/bin/$arch/Release/$lang/Gapotchenko.GnuTK.Setup.Msi.msi" "bin/$output_file_name"
+}
+
+# ---------------------------------------------------------------------------
+
+# Windows
+pack_windows windows-x64 x64
+pack_windows windows-arm64 ARM64
